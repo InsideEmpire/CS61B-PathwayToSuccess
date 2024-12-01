1fcc3ba16ce3f156cf02752e636f16fee43e2008
Garrison <garrison_liu@outlook.com>
2024-12-01 11:26:35 +0800

    finish BFS and cycle check

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,9 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Stack;
+
+import java.util.ArrayList;
+
 /**
  *  @author Josh Hug
  */
@@ -9,16 +13,64 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int[] parentOf;
 
     public MazeCycles(Maze m) {
         super(m);
+        parentOf = new int[maze.V()];
+        for (int i = 1; i < maze.V(); i += 1) {
+            parentOf[i] = Integer.MAX_VALUE;
+        }
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        dfsHelper();
     }
 
-    // Helper methods go here
+    private void dfsHelper() {
+
+        Stack<Integer> dfsStack = new Stack<>();
+        Stack<Integer> loopStack = new Stack<>();
+
+        dfsStack.push(0);
+        int curr;
+
+        while (!dfsStack.isEmpty()) {
+
+            curr = dfsStack.pop();
+            marked[curr] = true;
+            loopStack.push(curr);
+
+            announce();
+
+            for (int next : maze.adj(curr)) {
+                if (!marked[next]) {
+                    dfsStack.push(next);
+                    parentOf[next] = curr;
+                } else if (parentOf[curr] != next) {
+                    findCycle(loopStack, next);
+                    return;
+                }
+            }
+        }
+    }
+
+    private void findCycle(Stack<Integer> loopStack, int duplicate) {
+
+        int head = loopStack.peek();
+        edgeTo[head] = duplicate;
+        edgeTo[duplicate] = head;
+        announce();
+
+        while (!loopStack.isEmpty() && loopStack.peek() != duplicate) {
+            int out = loopStack.pop();
+            if (!loopStack.isEmpty()) {
+                edgeTo[out] = loopStack.peek();
+                announce();
+            }
+        }
+
+    }
 }
 
