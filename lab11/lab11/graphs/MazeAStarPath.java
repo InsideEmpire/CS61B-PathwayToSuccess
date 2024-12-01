831521612cebe632cc2ec66bfebdc93046ddd3af
Garrison Liu <garrison_liu@outlook.com>
2024-12-01 14:59:58 +0800

    finish Lab11 (#7)
    * finish BFS and cycle check
    
    * finish A*

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,7 +1,12 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.Comparator;
+
 /**
- *  @author Josh Hug
+ * @author Josh Hug
+ * @author Garrison
  */
 public class MazeAStarPath extends MazeExplorer {
     private int s;
@@ -18,20 +23,54 @@ public class MazeAStarPath extends MazeExplorer {
         edgeTo[s] = s;
     }
 
-    /** Estimate of the distance from v to the target. */
-    private int h(int v) {
-        return -1;
+    private class MinPQComparator implements Comparator {
+
+        @Override
+        public int compare(Object o1, Object o2) {
+            try {
+                int v1 = (java.lang.Integer) o1;
+                int v2 = (java.lang.Integer) o2;
+                return Math.abs(maze.toX(v1) - maze.toX(t)) + Math.abs(maze.toY(v1) - maze.toY(t))
+                        - Math.abs(maze.toX(v2) - maze.toX(t)) + Math.abs(maze.toY(v2) - maze.toY(t));
+            } catch (ClassCastException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
-    /** Finds vertex estimated to be closest to target. */
-    private int findMinimumUnmarked() {
-        return -1;
-        /* You do not have to use this method. */
-    }
+    /**
+     * Performs an A star search from vertex start.
+     */
+    private void astar(int start) {
+        MinPQ<Integer> pq = new MinPQ<>(new MinPQComparator());
+        pq.insert(start);
 
-    /** Performs an A star search from vertex s. */
-    private void astar(int s) {
-        // TODO
+        int curr;
+
+        while (!pq.isEmpty()) {
+
+            curr = pq.delMin();
+            marked[curr] = true;
+
+            for (int next : maze.adj(curr)) {
+                if (!marked[next]) {
+                    edgeTo[next] = curr;
+                    distTo[next] = distTo[curr] + 1;
+                    announce();
+                    pq.insert(next);
+                    if (targetFound) {
+                        return;
+                    }
+                }
+            }
+
+            if (curr == t) {
+                targetFound = true;
+            }
+            if (targetFound) {
+                return;
+            }
+        }
     }
 
     @Override
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,7 +1,10 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Stack;
+
 /**
  *  @author Josh Hug
+ * @author Garrison
  */
 public class MazeCycles extends MazeExplorer {
     /* Inherits public fields:
@@ -9,16 +12,64 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int[] parentOf;
 
     public MazeCycles(Maze m) {
         super(m);
+        parentOf = new int[maze.V()];
+        for (int i = 1; i < maze.V(); i += 1) {
+            parentOf[i] = Integer.MAX_VALUE;
+        }
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        dfsHelper(0);
     }
 
-    // Helper methods go here
+    private void dfsHelper(int start) {
+
+        Stack<Integer> dfsStack = new Stack<>();
+        Stack<Integer> loopStack = new Stack<>();
+
+        dfsStack.push(start);
+        int curr;
+
+        while (!dfsStack.isEmpty()) {
+
+            curr = dfsStack.pop();
+            marked[curr] = true;
+            loopStack.push(curr);
+
+            announce();
+
+            for (int next : maze.adj(curr)) {
+                if (!marked[next]) {
+                    dfsStack.push(next);
+                    parentOf[next] = curr;
+                } else if (parentOf[curr] != next) {
+                    findCycle(loopStack, next);
+                    return;
+                }
+            }
+        }
+    }
+
+    private void findCycle(Stack<Integer> loopStack, int duplicate) {
+
+        int head = loopStack.peek();
+        edgeTo[head] = duplicate;
+        edgeTo[duplicate] = head;
+        announce();
+
+        while (!loopStack.isEmpty() && loopStack.peek() != duplicate) {
+            int out = loopStack.pop();
+            if (!loopStack.isEmpty()) {
+                edgeTo[out] = loopStack.peek();
+                announce();
+            }
+        }
+
+    }
 }
 
