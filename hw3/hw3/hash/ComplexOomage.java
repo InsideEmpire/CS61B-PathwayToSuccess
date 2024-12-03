e60ee91a761cb69f868965f1c8a44c392c539ac2
Garrison Liu <garrison_liu@outlook.com>
2024-12-03 11:13:35 +0800

    finish Hw3 (#8)
    * finish equals
    
    * finish testHashCodePerfect
    
    * finish A Perfect hashCode
    
    * finish haveNiceHashCodeSpread
    
    * update HashCode
    
    * finish hw3
    
    * style check
    
    * fix testWithDeadlyParams and HashCode
    
    * fix style
    
    * fix a003
    
    * fix style

--- hw3/hw3/hash/ComplexOomage.java
+++ hw3/hw3/hash/ComplexOomage.java
@@ -3,6 +3,7 @@ import java.util.List;
 import java.util.ArrayList;
 import edu.princeton.cs.algs4.StdDraw;
 import java.awt.Color;
+
 import edu.princeton.cs.algs4.StdRandom;
 
 public class ComplexOomage implements Oomage {
@@ -13,8 +14,11 @@ public class ComplexOomage implements Oomage {
     public int hashCode() {
         int total = 0;
         for (int x : params) {
+            /*
             total = total * 256;
             total = total + x;
+            */
+            total = (total * 31 + x) ^ (total >>> 16);
         }
         return total;
     }
@@ -30,7 +34,7 @@ public class ComplexOomage implements Oomage {
 
     public ComplexOomage(List<Integer> params) {
         if (params == null) {
-            throw new IllegalArgumentException("params must not be null!");            
+            throw new IllegalArgumentException("params must not be null!");
         }
 
         for (Integer x : params) {
--- hw3/hw3/hash/TestComplexOomage.java
+++ hw3/hw3/hash/TestComplexOomage.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class TestComplexOomage {
 
@@ -33,18 +34,43 @@ public class TestComplexOomage {
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
     }
 
-    /* TODO: Create a list of Complex Oomages called deadlyList
+    /* Create a list of Complex Oomages called deadlyList
      * that shows the flaw in the hashCode function.
      */
-    /*
+
     @Test
     public void testWithDeadlyParams() {
         List<Oomage> deadlyList = new ArrayList<>();
 
-        // Your code here.
+        int N = 100;
+        int randomN = StdRandom.uniform(1, 10);
+        ArrayList<Integer> randomList = new ArrayList<>(randomN);
+        for (int i = 0; i < 1; i += 1) {
+            randomList.add(StdRandom.uniform(0, 255));
+        }
+
+        ArrayList<Integer> suffix = new ArrayList<>();
+        suffix.add(1);
+        suffix.add(2);
+        suffix.add(3);
+        suffix.add(4);
+        for (int i = 0; i < N; i++) {
+            ArrayList<Integer> params = randomList;
+            params.addAll(suffix);
+            //
+            for (Integer j : params) {
+                System.out.print(j);
+                System.out.print(" ");
+            }
+            System.out.println();
+            Oomage temp = new ComplexOomage(params);
+            System.out.println(temp.hashCode());
+            //
+            deadlyList.add(new ComplexOomage(params));
+        }
 
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 10));
-    } */
+    }
 
     /** Calls tests for SimpleOomage. */
     public static void main(String[] args) {
