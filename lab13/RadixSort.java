950a6bd69889898ecba006575e18512a4f459cb2
Garrison <garrison_liu@outlook.com>
2024-12-16 20:17:46 +0800

    fix bugs

--- lab13/RadixSort.java
+++ lab13/RadixSort.java
@@ -1,10 +1,9 @@
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 /**
  * Class for doing Radix sort
  *
  * @author Akhil Batra, Alexander Hwang
- *
  */
 public class RadixSort {
     /**
@@ -14,7 +13,6 @@ public class RadixSort {
      * The Strings can be variable length (all Strings are not constrained to 1 length)
      *
      * @param asciis String[] that needs to be sorted
-     *
      * @return String[] the sorted array
      */
     public static String[] sort(String[] asciis) {
@@ -46,35 +44,34 @@ public class RadixSort {
     /**
      * LSD helper method that performs a destructive counting sort the array of
      * Strings based off characters at a specific index.
+     *
      * @param asciis Input array of Strings
-     * @param index The position to sort the Strings on.
+     * @param index  The position to sort the Strings on.
      */
     private static String[] sortHelperLSD(String[] asciis, int index, int numDigitsInAnInteger) {
-        String[] sortedAsciis = new String[asciis.length];
-        LinkedList<String>[] sortedStr = new LinkedList[256];
-        for (int i = 0; i < asciis.length; i++) {
-            int leftMostIndex;
-            try {
-                leftMostIndex = (int) asciis[i].charAt(numDigitsInAnInteger - index - 1);
-            } catch (IndexOutOfBoundsException e) {
-                leftMostIndex = 0;
-            }
-            if (sortedStr[leftMostIndex] == null) {
-                sortedStr[leftMostIndex] = new LinkedList<>();
-            }
-            sortedStr[leftMostIndex].add(asciis[i]);
+        ArrayList<String>[] buckets = new ArrayList[256];
+        for (int i = 0; i < 256; i++) {
+            buckets[i] = new ArrayList<>();
         }
 
-        for (int i = 0, j = 0; i < 256; i++) {
-            if (sortedStr[i] == null) {
-                continue;
+        for (String str : asciis) {
+            int charIndex;
+            try {
+                charIndex = (int) str.charAt(numDigitsInAnInteger - index - 1);
+            } catch (IndexOutOfBoundsException e) {
+                charIndex = 0;
             }
-            for (String str : sortedStr[i]) {
-                sortedAsciis[j] = str;
-                j++;
+            buckets[charIndex].add(str);
+        }
+
+        int currentIndex = 0;
+        for (ArrayList<String> bucket : buckets) {
+            for (String str : bucket) {
+                asciis[currentIndex++] = str;
             }
         }
-        return sortedAsciis;
+
+        return asciis;
     }
 
     /**
@@ -82,10 +79,9 @@ public class RadixSort {
      * Destructive method that changes the passed in array, asciis.
      *
      * @param asciis String[] to be sorted
-     * @param start int for where to start sorting in this method (includes String at start)
-     * @param end int for where to end sorting in this method (does not include String at end)
-     * @param index the index of the character the method is currently sorting on
-     *
+     * @param start  int for where to start sorting in this method (includes String at start)
+     * @param end    int for where to end sorting in this method (does not include String at end)
+     * @param index  the index of the character the method is currently sorting on
      **/
     private static void sortHelperMSD(String[] asciis, int start, int end, int index) {
         // Optional MSD helper method for optional MSD radix sort
