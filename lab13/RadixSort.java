9a1cc1ec358306b9df1c5b9906a5cd962fb7906f
Garrison <garrison_liu@outlook.com>
2024-12-16 20:08:53 +0800

    finish Radix Sort

--- lab13/RadixSort.java
+++ lab13/RadixSort.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 /**
  * Class for doing Radix sort
  *
@@ -16,8 +18,29 @@ public class RadixSort {
      * @return String[] the sorted array
      */
     public static String[] sort(String[] asciis) {
-        // TODO: Implement LSD Sort
-        return null;
+        int numDigitsInAnInteger = 0;
+
+        for (int i = 0; i < asciis.length; i++) {
+            if (asciis[i].length() > numDigitsInAnInteger) {
+                numDigitsInAnInteger = asciis[i].length();
+            }
+        }
+
+        String[] sortedAsciis = asciis;
+
+        for (int i = 0; i < numDigitsInAnInteger; i++) {
+            sortedAsciis = sortHelperLSD(sortedAsciis, i, numDigitsInAnInteger);
+        }
+        // print(asciis);
+        // print(sortedAsciis);
+        return sortedAsciis;
+    }
+
+    private static void print(String[] strings) {
+        for (int i = 0; i < strings.length; i++) {
+            System.out.print(strings[i] + ' ');
+        }
+        System.out.println();
     }
 
     /**
@@ -26,9 +49,32 @@ public class RadixSort {
      * @param asciis Input array of Strings
      * @param index The position to sort the Strings on.
      */
-    private static void sortHelperLSD(String[] asciis, int index) {
-        // Optional LSD helper method for required LSD radix sort
-        return;
+    private static String[] sortHelperLSD(String[] asciis, int index, int numDigitsInAnInteger) {
+        String[] sortedAsciis = new String[asciis.length];
+        LinkedList<String>[] sortedStr = new LinkedList[256];
+        for (int i = 0; i < asciis.length; i++) {
+            int leftMostIndex;
+            try {
+                leftMostIndex = (int) asciis[i].charAt(numDigitsInAnInteger - index - 1);
+            } catch (IndexOutOfBoundsException e) {
+                leftMostIndex = 0;
+            }
+            if (sortedStr[leftMostIndex] == null) {
+                sortedStr[leftMostIndex] = new LinkedList<>();
+            }
+            sortedStr[leftMostIndex].add(asciis[i]);
+        }
+
+        for (int i = 0, j = 0; i < 256; i++) {
+            if (sortedStr[i] == null) {
+                continue;
+            }
+            for (String str : sortedStr[i]) {
+                sortedAsciis[j] = str;
+                j++;
+            }
+        }
+        return sortedAsciis;
     }
 
     /**
