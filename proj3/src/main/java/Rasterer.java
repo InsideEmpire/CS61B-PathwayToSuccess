6986654758864c7bf8116d7ae6a244858ac3c5ab
Garrison <garrison_liu@outlook.com>
2025-01-06 20:51:44 +0800

    Partial implementation of Rasterer: - Add LonDPP calculation for zoom levels. - Calculate ul/lr lon/lat positions for tiles. - Progress on determining best depth in getMapRaster.

--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -1,5 +1,4 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * This class provides all code necessary to take a query box and produce
@@ -8,9 +7,52 @@ import java.util.Map;
  * not draw the output correctly.
  */
 public class Rasterer {
+    private final List<Double> LonDPP_List = new LinkedList<>();
+    /*
+        Store the ul (or lr) lon (or lat) of each tiles at Dth zoom level
+        lon_List.get(Dth zoom level).get(x)
+        lat_List.get(Dth zoom level).get(y)
+     */
+    private final List<List<Double>> ullon_List = new LinkedList<>();
+    private final List<List<Double>> ullat_List = new LinkedList<>();
+    private final List<List<Double>> lrlon_List = new LinkedList<>();
+    private final List<List<Double>> lrlat_List = new LinkedList<>();
+
+    private final double TOTAL_LONGITUDE = 122.29980468 - 122.21191406;
+    private final double TOTAL_LATITUDE = 37.89219554 - 37.82280243;
+
+    private final double START_LONGITUDE = -122.29980468;
+    private final double END_LONGITUDE = -122.21191406;
+    private final double START_LATITUDE = 37.82280243;
+    private final double END_LATITUDE = 37.89219554;
+
+    private final double PIXEL = 256;
 
     public Rasterer() {
-        // YOUR CODE HERE
+        for (int Dth = 0; Dth <= 7; Dth++) {
+            /*
+                @num: How many tiles are in a row at the Dth zoom level
+                @DthLon: The longitude length of each tiles at Dth zoom level
+                @DthLat: The latitude length of each tiles at Dth zoom level
+                @DthLonDPP: The LonDPP of Dth zoom level tile
+             */
+            double num = Math.pow(2, Dth);
+            double DthLon = TOTAL_LONGITUDE / num;
+            double DthLat = TOTAL_LATITUDE / num;
+            double DthLonDPP = DthLon / PIXEL;
+            LonDPP_List.add(DthLonDPP);
+            ullon_List.add(new ArrayList<>());
+            lrlon_List.add(new ArrayList<>());
+            ullat_List.add(new ArrayList<>());
+            lrlat_List.add(new ArrayList<>());
+            for (int i = 0; i < num; i++) {
+                ullon_List.get(Dth).add(START_LONGITUDE + i * DthLon);
+                lrlon_List.get(Dth).add(START_LONGITUDE + (i + 1) * DthLon);
+                ullat_List.get(Dth).add(START_LATITUDE + i * DthLat);
+                lrlat_List.get(Dth).add(START_LATITUDE + (i + 1) * DthLat);
+            }
+            System.out.println("LonDPP of " + Dth + "th level is: " + DthLonDPP);
+        }
     }
 
     /**
@@ -42,10 +84,30 @@ public class Rasterer {
      *                    forget to set this to true on success! <br>
      */
     public Map<String, Object> getMapRaster(Map<String, Double> params) {
-        // System.out.println(params);
+         System.out.println(params);
         Map<String, Object> results = new HashMap<>();
-        System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
-                           + "your browser.");
+        double lrlon = params.get("lrlon");
+        double ullon = params.get("ullon");
+        double ullat = params.get("ullat");
+        double lrlat = params.get("lrlat");
+        double w = params.get("ullon");
+        double h = params.get("ullon");
+        double LonDPP = (lrlon - ullon) / w;
+        System.out.println(LonDPP);
+
+        int depth = 0;
+        for (int Dth = 0; Dth <= 7; Dth++) {
+            if (LonDPP_List.get(Dth) < LonDPP) {
+                depth = Dth - 1;
+                break;
+            }
+        }
+        System.out.println(depth + " is the BEST zoom level");
+
+        // TODO: finish render_grid...
+        // TODO: (int) Math.pow(2, depth) is not good answer. Try to calculate Query Box's position
+        String[][] render_grid = new String[(int) Math.pow(2, depth)][(int) Math.pow(2, depth)];
+
         return results;
     }
 
