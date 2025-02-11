d79c6957b1d7a1139f92830e0e1bbf66b28d660c
Garrison <garrison_liu@outlook.com>
2025-02-11 10:43:33 +0800

    finish HuffmanEncoder

---
+++ hw7/HuffmanEncoder.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HuffmanEncoder {
+    public static Map<Character, Integer> buildFrequencyTable(char[] inputSymbols) {
+        Map<Character, Integer> frequencyTable = new HashMap<>();
+        for (int i = 0; i < inputSymbols.length; i++) {
+            frequencyTable.put(inputSymbols[i], frequencyTable.getOrDefault(inputSymbols[i], 0) + 1);
+        }
+        return frequencyTable;
+    }
+    public static void main(String[] args) {
+        // 1: Read the file as 8 bit symbols.
+        String fileName = args[0];
+        char[] inputSymbols = FileUtils.readFile(fileName);
+        // 2: Build frequency table.
+        Map<Character, Integer> frequencyTable = buildFrequencyTable(inputSymbols);
+        // 3: Use frequency table to construct a binary decoding trie.
+        BinaryTrie huffmanTree = new BinaryTrie(frequencyTable);
+        // 4: Write the binary decoding trie to the .huf file.
+        String newFileName = fileName + ".huf";
+        ObjectWriter objectWriter = new ObjectWriter(newFileName);
+        objectWriter.writeObject(huffmanTree);
+        // 6: Use binary trie to create lookup table for encoding.
+        Map<Character, BitSequence> lookupTable = huffmanTree.buildLookupTable();
+        // 7: Create a list of bitsequences.
+        List<BitSequence> bitSequenceList = new ArrayList<>();
+        // 8: For each 8 bit symbol:
+        //    Lookup that symbol in the lookup table.
+        //    Add the appropriate bit sequence to the list of bitsequences.
+        for (Character ch : lookupTable.keySet()) {
+            bitSequenceList.add(lookupTable.get(ch));
+        }
+        // 9: Assemble all bit sequences into one huge bit sequence.
+        BitSequence hugeBitSequence = new BitSequence(BitSequence.assemble(bitSequenceList));
+        // 10: Write the huge bit sequence to the .huf file.
+        objectWriter.writeObject(hugeBitSequence);
+    }
+}
